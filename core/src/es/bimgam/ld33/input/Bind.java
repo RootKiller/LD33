313056aa4e6bd2fb169410ab138c36d64bc39663
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2015-08-20 04:20:48 +0200

    - input: added ability to run command from bind.

--- core/src/es/bimgam/ld33/input/Bind.java
+++ core/src/es/bimgam/ld33/input/Bind.java
@@ -1,17 +1,38 @@
 package es.bimgam.ld33.input;
 
 import com.badlogic.gdx.Gdx;
+import es.bimgam.ld33.core.CommandManager;
 
 /**
  * Created by Eryk on 20.08.2015.
  */
 public class Bind {
-	int key;
-	boolean down;
+	private int key;
+	private boolean down;
+	private boolean hasCommand;
+	private String command;
 
 	public Bind(int key, boolean down) {
 		this.key = key;
 		this.down = down;
+		this.hasCommand = false;
+	}
+
+	public Bind(int key, boolean down, String command) {
+		this.key = key;
+		this.down = down;
+		this.hasCommand = true;
+		this.command = command;
+	}
+
+	public void tick() {
+		if (! this.hasCommand) {
+			return;
+		}
+
+		if (isActive()) {
+			CommandManager.Instance.execute(this.command);
+		}
 	}
 
 	public boolean isActive() {
