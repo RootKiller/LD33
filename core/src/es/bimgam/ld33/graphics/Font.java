cec603f372f254f4dff8591066c53f024a90319f
pcpl2 <patryykk12@gmail.com>
2015-08-21 00:45:35 +0200

    - graphics: added Font class that implements FreeType font generating

---
+++ core/src/es/bimgam/ld33/graphics/Font.java
@@ -0,0 +1,66 @@
+package es.bimgam.ld33.graphics;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.Color;
+
+public class Font {
+
+    public static final String POLISH_CHARACTERS = "AĄBCĆDEĘGHIJKLŁMNŃOÓPQRSŚTUVWXYZŻŹ"
+            + "aąbcćdeęghijklłmnńoópqrsśtuvwxyzżź"
+            + "1234567890.,:;_¡!?\"'+-*/()[]={}";
+
+    private BitmapFont font;
+
+    private GlyphLayout glyphLayout;
+
+    public Font(String fontName, Integer size) {
+
+        FreeTypeFontGenerator.FreeTypeFontParameter fontParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        fontParameter.size = size;
+        fontParameter.characters = POLISH_CHARACTERS;
+
+        FreeTypeFontGenerator fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal(fontName));
+        font = fontGenerator.generateFont(fontParameter);
+
+        fontGenerator.dispose();
+        glyphLayout = new GlyphLayout();
+    }
+
+    public void draw(Batch batch, CharSequence str, float x, float y) {
+        glyphLayout.setText(font, str);
+        font.draw(batch, glyphLayout, x, y);
+    }
+
+    public void draw(Batch batch, CharSequence str, float x, float y, Color color, float targetWidth, int halign, boolean wrap) {
+        glyphLayout.setText(font, str, color, targetWidth, halign, wrap);
+        font.draw(batch, glyphLayout, x, y);
+    }
+
+    public float getRenderWidth(CharSequence str) {
+        glyphLayout.setText(font, str);
+        return glyphLayout.width;
+    }
+
+    public float getRenderWidth(CharSequence str, float targetWidth, int halign, boolean wrap) {
+        glyphLayout.setText(font, str, Color.WHITE, targetWidth, halign, wrap);
+        return glyphLayout.width;
+    }
+
+    public float getRenderHeight(CharSequence str) {
+        glyphLayout.setText(font, str);
+        return glyphLayout.height;
+    }
+
+    public float getRenderHeight(CharSequence str, float targetWidth, int halign, boolean wrap) {
+        glyphLayout.setText(font, str, Color.WHITE, targetWidth, halign, wrap);
+        return glyphLayout.height;
+    }
+    
+    public void dispose() {
+        font.dispose();
+    }
+}
