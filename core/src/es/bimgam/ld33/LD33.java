76f5cc4133089d538eeba9ea138ea60d40c64907
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2015-08-20 03:50:13 +0200

    - general: initial project commit

---
+++ core/src/es/bimgam/ld33/LD33.java
@@ -0,0 +1,31 @@
+package es.bimgam.ld33;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class LD33 extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+
+	Graphics graphics;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("interface/horny_peppers_logo.png");
+		graphics = Gdx.graphics;
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0, graphics.getWidth(), graphics.getHeight());
+		batch.end();
+	}
+}
---
+++ desktop/src/es/bimgam/ld33/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package es.bimgam.ld33.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import es.bimgam.ld33.LD33;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD33(), config);
+	}
+}
