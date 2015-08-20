00d1b2b802df97a4eb8a6f41113f07c67e55c356
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2015-08-20 15:28:19 +0200

    - ld33: fixed memory leaks and added implementation of AssetManager.

--- core/src/es/bimgam/ld33/LD33.java
+++ core/src/es/bimgam/ld33/LD33.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -14,13 +15,15 @@ import es.bimgam.ld33.input.BindPool;
 import es.bimgam.ld33.core.CommandManager;
 
 public class LD33 extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private Texture img;
 
-	Graphics graphics;
+	private Graphics graphics;
 
 	private CommandManager commandManager;
 	private BindPool bindPool;
+
+	private AssetManager assetsManager;
 
 	public static LD33 Instance = null;
 
@@ -33,7 +36,8 @@ public class LD33 extends ApplicationAdapter {
 		bindPool = new BindPool();
 		commandManager = new CommandManager();
 		batch = new SpriteBatch();
-		img = new Texture("interface/horny_peppers_logo.png");
+		assetsManager = new AssetManager();
+		assetsManager.load("interface/horny_peppers_logo.png", Texture.class);
 		graphics = Gdx.graphics;
 
 		commandManager.register(new QuitCommand());
@@ -43,13 +47,28 @@ public class LD33 extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		// TODO: Find better place to handle all the non-graphical stuffs
+		if (assetsManager.update()) {
+			img = assetsManager.get("interface/horny_peppers_logo.png");
+		}
 		bindPool.tick();
 
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0, graphics.getWidth(), graphics.getHeight());
-		batch.end();
+
+		if (img != null) {
+			batch.begin();
+			batch.draw(img, 0, 0, graphics.getWidth(), graphics.getHeight());
+			batch.end();
+		}
+	}
+
+	@Override
+	public void dispose () {
+		batch.dispose();
+		assetsManager.dispose();
+	}
+
+	public AssetManager getAssetsManager() {
+		return assetsManager;
 	}
 }
