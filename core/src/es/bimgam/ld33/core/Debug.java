3cf6b7218455a5c0bab962d85d78c4730d998910
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2015-08-22 20:45:12 +0200

    - core: added Debug helper class

---
+++ core/src/es/bimgam/ld33/core/Debug.java
@@ -0,0 +1,19 @@
+package es.bimgam.ld33.core;
+
+public class Debug {
+	public static final boolean RENDER_PHYSICS = false;
+
+	public static void Log(String message) {
+		System.out.println("[debug] " + message);
+	}
+
+	public static void Log(Object object) {
+		System.out.println("[debug] " + object.toString());
+	}
+
+	public static void Assert(boolean condition, String message) {
+		if (! condition) {
+			Log("[assert] " + message);
+		}
+	}
+}
