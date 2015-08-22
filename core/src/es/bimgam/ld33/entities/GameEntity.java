7efa04583f1295f909fd495c4dcf883e425f9990
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2015-08-22 20:46:03 +0200

    - entities: extended GameEntity functionallity

--- core/src/es/bimgam/ld33/entities/GameEntity.java
+++ core/src/es/bimgam/ld33/entities/GameEntity.java
@@ -3,6 +3,7 @@ package es.bimgam.ld33.entities;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.World;
 
@@ -10,10 +11,37 @@ public class GameEntity {
 
 	public Body physicalBody;
 	public Sprite sprite;
+	public Scene scene;
+
+	private boolean isAlive;
+	private World physicalWorld;
 
 	public GameEntity(Scene scene, World physicalWorld, AssetManager assetManager) {
+		this.scene = scene;
+		this.isAlive = true;
+		this.physicalWorld = physicalWorld;
+
 		setupPhysics(physicalWorld);
 		setupVisuals(assetManager);
+	}
+
+	public String getTypeName() {
+		return "GameEntity";
+	}
+
+	public void dispose() {
+		if (this.physicalBody != null) {
+			this.physicalWorld.destroyBody(this.physicalBody);
+			this.physicalBody = null;
+		}
+	}
+
+	public void destroy() {
+		this.isAlive = false;
+	}
+
+	public boolean isAlive() {
+		return this.isAlive;
 	}
 
 	public void setupPhysics(World physicalWorld) {
@@ -29,5 +57,15 @@ public class GameEntity {
 	}
 
 	public void render(SpriteBatch batch) {
+	}
+
+	public void onCollisionEnter(GameEntity entity) {
+	}
+
+	public void onCollisionExit(GameEntity entity) {
+	}
+
+	public Vector2 getPosition() {
+		return Vector2.Zero;
 	}
 }
