67f34ae8a2f8ac910130f228f80ca179c6d5219c
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2015-08-24 01:21:27 +0200

    - core: new debug constants TEST_UI_MENU and ALLOW_CHEATS

--- core/src/es/bimgam/ld33/core/Debug.java
+++ core/src/es/bimgam/ld33/core/Debug.java
@@ -2,6 +2,8 @@ package es.bimgam.ld33.core;
 
 public class Debug {
 	public static final boolean RENDER_PHYSICS = false;
+	public static final boolean TEST_UI_MENU = false;
+	public static final boolean ALLOW_CHEATS = false;
 
 	public static void Log(String message) {
 		System.out.println("[debug] " + message);
