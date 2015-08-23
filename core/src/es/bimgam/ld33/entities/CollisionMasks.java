d54f65e7e628b576b23148a2b40fd0c2a6a29efc
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2015-08-23 23:49:02 +0200

    - entities: collision mask for enemy bullet

--- core/src/es/bimgam/ld33/entities/CollisionMasks.java
+++ core/src/es/bimgam/ld33/entities/CollisionMasks.java
@@ -1,8 +1,9 @@
 package es.bimgam.ld33.entities;
 
 public class CollisionMasks {
-	public final static short PLAYER	= 0x0001; // 0001
-	public final static short ENEMY		= 0x0002; // 0010
-	public final static short BULLET	= 0x0004; // 0100
-	public final static short PICKUP	= 0x0008; // 1000
+	public final static short PLAYER	= 0x0001; // 00001
+	public final static short ENEMY		= 0x0002; // 00010
+	public final static short BULLET	= 0x0004; // 00100
+	public final static short ENEMY_BULLET	= 0x0008; // 01000
+	public final static short PICKUP	= 0x0010; // 10000
 }
