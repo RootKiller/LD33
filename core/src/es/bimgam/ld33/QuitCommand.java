cb0e2155fd17acfad7239d446e64acedf8522fe4
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2015-08-24 18:44:01 +0200

    - ld33: save game when exiting by pressing ESC

--- core/src/es/bimgam/ld33/QuitCommand.java
+++ core/src/es/bimgam/ld33/QuitCommand.java
@@ -2,6 +2,8 @@ package es.bimgam.ld33;
 
 import com.badlogic.gdx.Gdx;
 import es.bimgam.ld33.core.Command;
+import es.bimgam.ld33.states.InGameState;
+import es.bimgam.ld33.states.StateManager;
 
 public class QuitCommand extends Command {
 	public QuitCommand() {
@@ -10,6 +12,10 @@ public class QuitCommand extends Command {
 
 	@Override
 	public void run() {
+		InGameState igs = StateManager.Instance.getActiveStateSafe(InGameState.class);
+		if (igs != null) {
+			igs.save();
+		}
 		Gdx.app.exit();
 	}
 }
